9fa357dcf9b2f19b4ef89a749818876319b549a6
Simona Lorenc <lorenc.simona@gmail.com>
2023-07-11 16:05:53 +0200

    addHamburgerMenu

--- main.js
+++ main.js
@@ -1,32 +1,85 @@
 const headerMenu = document.querySelector('.header__menu')
+const header = document.querySelector('.header__text')
 
 const hamburgerLogo = document.createElement('div')
 hamburgerLogo.classList.add('hamburger-logo')
 
-// const hamburgerMenu = document.querySelector('.header__nav-menu')
-// const header = document.querySelector('.header')
-// const headerMenu = document.querySelector('.header__menu')
+const arrowNext = document.createElement('div')
+arrowNext.classList.add('arrow-next')
+const arrowPrevious = document.createElement('div')
+arrowPrevious.classList.add('arrow-previous')
 
-// const hamburgerLogo = document.createElement('div')
-// hamburgerLogo.classList.add('hamburger-logo')
+const bigContainerForPhoto = document.querySelector('.photo__big')
 
-// if(window.innerWidth <= 550) {
-//     addHamburgerLogo()
-// }
+if(window.innerWidth <= 1220) {
+    addHamburgerLogo()
+}
 
-// const hideMenu = () => {
-//     if (window.innerWidth <= 550) {
-//         addHamburgerLogo()
-//     } else {
-//         removeHamburgerLogo()
-//     }
-// }
+if(window.innerWidth <= 650) {
+    addArrows()
+}
 
-// window.addEventListener('resize', hideMenu)
+const hideMenu = () => {
+    if (window.innerWidth <= 1220) {
+        addHamburgerLogo()
+    } else {
+        removeHamburgerLogo()
+    }
+}
 
-// function addHamburgerLogo() {
-//     hamburgerMenu.style.display = 'none'
-//     hamburgerMenu.classList.remove('header__nav-menu')
-//     header.style.justifyContent = 'space-between'
-//     header.appendChild(hamburgerLogo)
-// }
+window.addEventListener('resize', hideMenu)
+
+function addHamburgerLogo() {
+    headerMenu.style.display = 'none'
+    header.insertBefore(hamburgerLogo, header.firstChild)
+}
+
+function removeHamburgerLogo() {
+    headerMenu.style.display = 'flex'
+    header.removeChild(hamburgerLogo)
+}
+
+function addArrows() {
+    bigContainerForPhoto.append(arrowNext)
+    bigContainerForPhoto.append(arrowPrevious)
+}
+
+function hideArrows() {
+    bigContainerForPhoto.removeChild(arrowNext)
+    bigContainerForPhoto.removeChild(arrowPrevious)
+}
+
+const addOrHideArrows = () => {
+    if (window.innerWidth <= 650) {
+        addArrows()
+    } else {
+        hideArrows()
+    }
+}
+
+window.addEventListener('resize', addOrHideArrows)
+
+const smallPhotos = document.querySelectorAll('.small')
+console.log(smallPhotos)
+
+function unselectedPhoto() {
+    smallPhotos.forEach(smallPhoto => smallPhoto.classList.remove('selected-photo'))
+}
+
+smallPhotos.forEach(smallPhoto => smallPhoto.addEventListener('click', () => {
+    unselectedPhoto()
+    smallPhoto.classList.add('selected-photo')
+    const bigImage = document.querySelector('.big')
+    const selectedImage = document.querySelector('.selected-photo')
+    console.log(selectedImage.src)
+    const newSelectedImage = selectedImage.src
+    bigImage.src = selectedImage.src
+    })
+)
+
+arrowNext.addEventListener('click', () => {
+    const bigImage = document.querySelector('.big')
+    //tablica ze zdjeciami
+    console.log(bigImage)
+    console.log('arrow next clicked')
+})
