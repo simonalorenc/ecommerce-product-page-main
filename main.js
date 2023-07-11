9a484e75f421a6125f264815fcc0c33834bc6d37
Simona Lorenc <lorenc.simona@gmail.com>
2023-07-11 08:33:27 +0200

    readyMobileStaticPage

---
+++ main.js
@@ -0,0 +1,32 @@
+const headerMenu = document.querySelector('.header__menu')
+
+const hamburgerLogo = document.createElement('div')
+hamburgerLogo.classList.add('hamburger-logo')
+
+// const hamburgerMenu = document.querySelector('.header__nav-menu')
+// const header = document.querySelector('.header')
+// const headerMenu = document.querySelector('.header__menu')
+
+// const hamburgerLogo = document.createElement('div')
+// hamburgerLogo.classList.add('hamburger-logo')
+
+// if(window.innerWidth <= 550) {
+//     addHamburgerLogo()
+// }
+
+// const hideMenu = () => {
+//     if (window.innerWidth <= 550) {
+//         addHamburgerLogo()
+//     } else {
+//         removeHamburgerLogo()
+//     }
+// }
+
+// window.addEventListener('resize', hideMenu)
+
+// function addHamburgerLogo() {
+//     hamburgerMenu.style.display = 'none'
+//     hamburgerMenu.classList.remove('header__nav-menu')
+//     header.style.justifyContent = 'space-between'
+//     header.appendChild(hamburgerLogo)
+// }
