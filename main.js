35e1ab6710f55d25e6cf73431ec958e5678ac91f
Simona Lorenc <lorenc.simona@gmail.com>
2023-07-12 14:01:03 +0200

    addMobileMenuAndPopup

--- main.js
+++ main.js
@@ -4,10 +4,15 @@ const header = document.querySelector('.header__text')
 const hamburgerLogo = document.createElement('div')
 hamburgerLogo.classList.add('hamburger-logo')
 
+const closeMenu = document.createElement('div')
+closeMenu.classList.add('close-menu')
+
 const arrowNext = document.createElement('div')
 arrowNext.classList.add('arrow-next')
 const arrowPrevious = document.createElement('div')
 arrowPrevious.classList.add('arrow-previous')
+
+const menuOptions = document.querySelectorAll('.header__menu-option')
 
 const bigContainerForPhoto = document.querySelector('.photo__big')
 
@@ -35,9 +40,33 @@ function addHamburgerLogo() {
 }
 
 function removeHamburgerLogo() {
-    headerMenu.style.display = 'flex'
-    header.removeChild(hamburgerLogo)
+    if (header.contains(hamburgerLogo)) {
+        header.removeChild(hamburgerLogo);
+    }
+    headerMenu.style.display = 'flex';
 }
+
+hamburgerLogo.addEventListener('click', () => {
+    headerMenu.classList.add('mobile-menu')
+    headerMenu.style.display = 'flex'
+    menuOptions.forEach((option) => {
+        option.style.color = 'black'
+        option.style.fontWeight = '700'
+    })
+    headerMenu.appendChild(closeMenu)
+})
+
+closeMenu.addEventListener('click', () => {
+    if (headerMenu.contains(closeMenu)) {
+        headerMenu.removeChild(closeMenu)
+    }
+    menuOptions.forEach((option) => {
+        option.style.color = 'var(--color-text)'
+        option.style.fontWeight = '400'
+    })
+    headerMenu.classList.remove('mobile-menu')
+    headerMenu.style.display = 'none'
+})
 
 function addArrows() {
     bigContainerForPhoto.append(arrowNext)
@@ -45,8 +74,12 @@ function addArrows() {
 }
 
 function hideArrows() {
-    bigContainerForPhoto.removeChild(arrowNext)
-    bigContainerForPhoto.removeChild(arrowPrevious)
+    if (bigContainerForPhoto.contains(arrowNext)) {
+        bigContainerForPhoto.removeChild(arrowNext)
+    }
+    if (bigContainerForPhoto.contains(arrowPrevious)) {
+        bigContainerForPhoto.removeChild(arrowPrevious)
+    }
 }
 
 const addOrHideArrows = () => {
@@ -60,7 +93,6 @@ const addOrHideArrows = () => {
 window.addEventListener('resize', addOrHideArrows)
 
 const smallPhotos = document.querySelectorAll('.small')
-console.log(smallPhotos)
 
 function unselectedPhoto() {
     smallPhotos.forEach(smallPhoto => smallPhoto.classList.remove('selected-photo'))
@@ -96,6 +128,61 @@ arrowNext.addEventListener('click', () => {
         index = 0
         activeSlide.src = slidesArray[index]
     }
+})
+
+arrowPrevious.addEventListener('click', () => {
+    let activeSlide = document.querySelector('.big')
+    let index = currentIndexOfPhoto() - 1
+    activeSlide.src = slidesArray[index]
+    if (index < 0) {
+        index = 3
+        activeSlide.src = slidesArray[index]
+    }
+})
+
+const popup = document.querySelector('.popup')
+const bigPhoto = document.querySelector('.big')
+
+const arrowNextPopup = document.createElement('div')
+arrowNextPopup.classList.add('arrow-next')
+const arrowPreviousPopup = document.createElement('div')
+arrowPreviousPopup.classList.add('arrow-previous')
+
+const activePopupPhoto = document.querySelector('.popup-big')
+
+function addArrowsForPopup() {
+    popup.appendChild(arrowNextPopup)
+    popup.appendChild(arrowPreviousPopup)
+}
+
+bigPhoto.addEventListener('click', () => {
+    popup.classList.remove('hidden')
+    addArrowsForPopup()
+})
+
+arrowNextPopup.addEventListener('click', () => {
+    const bigPopupPhoto = activePopupPhoto.src
+    let currentIndex = slidesArray.indexOf(bigPopupPhoto)
+    let index = currentIndex + 1
+    activePopupPhoto.src = slidesArray[index]
+    if (index > 3) {
+        index = 0
+        activePopupPhoto.src = slidesArray[index]
+    }
+})
+
+arrowPreviousPopup.addEventListener('click', () => {
+    const bigPopupPhoto = activePopupPhoto.src
+    let currentIndex = slidesArray.indexOf(bigPopupPhoto)
+    console.log(currentIndex)
+    let index = currentIndex - 1
+    console.log(index)
+    if (index < 0) {
+        index = 3
+        activePopupPhoto.src = slidesArray[index]
+        console.log(index)
+    }
+    activePopupPhoto.src = slidesArray[index]
 })
 
 
